0630448043a42c67ae8fe80517708773c8bfb3c2
Dimahtml <bomabo@mail.ru>
2018-12-18 15:42:09 +0500

    при успешной отправке данных показывается попап. (пункт 3.3)

--- js/scale-of-image.js
+++ js/scale-of-image.js
@@ -1,5 +1,6 @@
 'use strict';
-// этот модуль показывает и закрывет загружаемую фотографию (при нажатии кнопки ЗАГРУЗИТЬ)
+// этот модуль показывает и закрывет загружаемую фотографию (при нажатии кнопки ЗАГРУЗИТЬ) и
+// отправляет данные на сервер
 
 // закрываем и очищаем форму
 var clearForm = function () {
@@ -60,16 +61,46 @@ var clearForm = function () {
 
   });
 
+  // успешная отправка данных. показываем попап. закрываем попап.
   var upLoadSuccess  = function (message) {
-
     clearForm ();
-    // alert(message);
+    // шаблон, который будем копировать
+    var template = document.querySelector('#success').content.querySelector('section');
+    // создаем окно
+    var popup = template.cloneNode(true);
+    // куда будем вставлять
+    var main = document.querySelector('main');
+    // вставляем
+    main.appendChild(popup);
+
+    var handlerClick = function () {
+      main.removeChild(popup);
+      document.removeEventListener('click', handlerClick);
+      window.removeEventListener('click', handlerClick);
+      document.removeEventListener('keydown', handlerESC);
+    };
+
+    var handlerESC = function () {
+      if ((evt.keyCode === window.constants.ESC_KEYCODE) && (!(window.isfocusedOnField === 1))) {
+        main.removeChild(popup);
+      }
+      document.removeEventListener('click', handlerClick);
+      window.removeEventListener('click', handlerClick);
+      document.removeEventListener('keydown', handlerESC);
+    };
+
+    // обработчик на закрытие по клику мыши на кнопку
+    document.querySelector('.success__button').addEventListener('click', handlerClick);
+    // обработчик на закрытие по клику мыши на произвольную область экрана
+    window.addEventListener('click', handlerClick);
+    // обработчик на закрытие по нажатию ESC
+    document.addEventListener('keydown', handlerESC);
+
   };
 
   var upLoadError = function (message) {
     // alert(message);
   };
-
 
   imgUploadOverlay.classList.add('hidden');
       uploadFile.value = '';// сбрасываем значение кнопки "загрузить" (для повторного открытия)
